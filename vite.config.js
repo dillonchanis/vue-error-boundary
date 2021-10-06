92d37f4596f44bcb2ee141fe9a06ee5883d2ea06
Dillon Chanis <dillonchanis92@gmail.com>
2021-10-06 19:00:06 -0400

    Generating types for component

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,30 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import dts from 'vite-plugin-dts'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/VErrorBoundary.vue'),
-      name: 'VErrorBoundary',
-      fileName: (format) => `vue-error-boundary.${format}.js`
+      entry: path.resolve(__dirname, "src/VErrorBoundary.vue"),
+      name: "VErrorBoundary",
+      fileName: (format) => `vue-error-boundary.${format}.js`,
     },
     rollupOptions: {
-      external: ['vue'],
+      external: ["vue"],
       output: {
         globals: {
-          vue: 'Vue'
-        }
-      }
-    }
+          vue: "Vue",
+        },
+      },
+    },
   },
-  plugins: [vue()]
-})
+  plugins: [
+    vue(),
+    dts({
+      outputDir: 'dist',
+      cleanVueFileName: true
+    }),
+  ],
+});
